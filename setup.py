f114e208937ac2f7b8a1507a3e4bb064d8ac7e21
Matthew Kaufer <mjkaufer@gmail.com>
2024-01-21 17:09:50 -0800

    bump

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
-from setuptools import setup 
-  
-setup( 
-    name='gaussian_splatting', 
-    version='0.1', 
-    description='Splatting for Modal experiment', 
-    author='Matthew Kaufer', 
-    author_email='mjkaufer@gmail.com', 
-    packages=['.'], 
-    install_requires=[ 
-        'numpy', 
-        'pandas', 
-    ], 
-) 
+from setuptools import setup, find_packages
+
+
+setup(
+    name="gaussian_splatting",
+    version="0.1",
+    description="Splatting for Modal experiment",
+    author="Matthew Kaufer",
+    author_email="mjkaufer@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+    ],
+)
